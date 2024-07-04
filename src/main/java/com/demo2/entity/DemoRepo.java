b4722f55bec2444c9c9928c5d53199a5a2eb1c4b
Umesh Kumarclear <umesh@technojade.com>
2024-07-04 08:27:37 +0530

    DemoRepo

---
+++ src/main/java/com/demo2/entity/DemoRepo.java
@@ -0,0 +1,18 @@
+package com.demo2.entity;
+
+
+
+public class DemoRepo {
+	
+	private Long gitId;
+
+	public Long getGitId() {
+		return gitId;
+	}
+
+	public void setGitId(Long gitId) {
+		this.gitId = gitId;
+	}
+
+	
+}
